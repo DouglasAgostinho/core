cff385584e81517e1cc809ff08adb4c3d09bd2b8
DouglasAgostinho <doug>
2023-11-29 15:02:14 -0300

    Added imports & Netwrok class with __init__

--- src/server.py
+++ src/server.py
@@ -14,3 +14,32 @@
 # 64 char for message hash (SHA-512)
 # 
 #------------------------------------------------------
+
+#Import section
+import queue
+import socket
+import select
+from Crypto.Hash import SHA512
+from Crypto.Random import random
+import sys
+
+
+class Network():
+    def __init__(self, host=None, port=None):
+        #Variable and constants definition
+        if host is None:
+            self.HOST = "localhost"
+        else:
+            self.HOST = host
+
+        if port is None:
+            self.PORT = 65440
+        else:
+            self.PORT = port
+
+        #Network constants
+        self.FORMAT = "utf-8"        
+        self.HEADER = 16
+        self.PARTICIPANTS = 100 #Be aware with system max of file descriptors
+        self.MSG_CONFIRMED = b"![MESSAGE CONFIRMED]!"
+        self.MSG_CORRUPTED = b"![MESSAGE CORRUPTED]!"
